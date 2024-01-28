e1178eda47bc5cf070d6ce0459643c242b826ef0
barroit <sunjiamu@outlook.com>
2024-01-29 07:57:37 +0900

    init

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#include <windows.h>
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+{
+	//
+	return 0;
+}
